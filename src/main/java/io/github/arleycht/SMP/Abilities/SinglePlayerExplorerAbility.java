6f6d366e73486b67cd5f610310edc6b1681f1f09
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-02-28 03:09:15 -0500

    Create SinglePlayerExplorerAbility.java
    Still WIP

---
+++ src/main/java/io/github/arleycht/SMP/Abilities/SinglePlayerExplorerAbility.java
@@ -0,0 +1,34 @@
+package io.github.arleycht.SMP.Abilities;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityToggleGlideEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+
+public class SinglePlayerExplorerAbility extends Ability {
+    protected boolean gliding = false;
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        EquipmentSlot hand = event.getHand();
+        ItemStack heldItemStack = player.getInventory().getItem(hand);
+        Material heldItemType = heldItemStack.getType();
+
+        gliding = !gliding;
+
+        if (hand == EquipmentSlot.HAND && heldItemType == Material.STICK) {
+            player.setGliding(gliding);
+        }
+    }
+
+    @EventHandler
+    public void onGlide(EntityToggleGlideEvent event) {
+        if (event.isGliding() != gliding) {
+            event.setCancelled(true);
+        }
+    }
+}

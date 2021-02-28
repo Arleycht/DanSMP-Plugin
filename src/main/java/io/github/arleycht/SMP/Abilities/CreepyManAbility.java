ac15bb52c061b6ffd28a21bf4e99913328178287
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-02-27 20:10:19 -0500

    Implement silent footsteps to CreepyManAbility

--- src/main/java/io/github/arleycht/SMP/Abilities/CreepyManAbility.java
+++ src/main/java/io/github/arleycht/SMP/Abilities/CreepyManAbility.java
@@ -1,6 +1,16 @@
 package io.github.arleycht.SMP.Abilities;
 
-import org.bukkit.*;
+import com.comphenix.packetwrapper.WrapperPlayServerNamedSoundEffect;
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -17,10 +27,59 @@ public class CreepyManAbility extends Ability {
     protected static final long CHECK_INTERVAL_TICKS = 20L * 60L;
 
     protected long lastGenerationTime;
+    protected PacketAdapter packetAdapter;
 
     @Override
     public void initialize() {
         lastGenerationTime = System.currentTimeMillis();
+
+        if (packetAdapter != null) {
+            ProtocolLibrary.getProtocolManager().removePacketListener(packetAdapter);
+        }
+
+        packetAdapter = new PacketAdapter(plugin, ListenerPriority.HIGHEST, PacketType.Play.Server.NAMED_SOUND_EFFECT) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                PacketContainer packet = event.getPacket();
+                Player receivingPlayer = event.getPlayer();
+                World world = receivingPlayer.getWorld();
+                WrapperPlayServerNamedSoundEffect soundPacket = new WrapperPlayServerNamedSoundEffect(packet);
+                String soundName = soundPacket.getSoundEffect().name().toLowerCase();
+
+                // If the sound is going to the owner, we can ignore this packet
+                if (isOwner(receivingPlayer.getUniqueId())) {
+                    return;
+                }
+
+                if (!soundName.contains("step")) {
+                    return;
+                }
+
+                double x = soundPacket.getEffectPositionX() / 8.0;
+                double y = soundPacket.getEffectPositionY() / 8.0;
+                double z = soundPacket.getEffectPositionZ() / 8.0;
+
+                Location soundLocation = new Location(world, x, y, z);
+
+                Player closestPlayer = null;
+                double closestDistanceSquared = Double.MAX_VALUE;
+
+                for (Player p : world.getPlayers()) {
+                    double distanceSquared = soundLocation.distanceSquared(p.getLocation());
+
+                    if (distanceSquared < closestDistanceSquared) {
+                        closestPlayer = p;
+                        closestDistanceSquared = distanceSquared;
+                    }
+                }
+
+                if (closestPlayer != null && isOwner(closestPlayer.getUniqueId())) {
+                    event.setCancelled(true);
+                }
+            }
+        };
+
+        ProtocolLibrary.getProtocolManager().addPacketListener(packetAdapter);
     }
 
     @Override

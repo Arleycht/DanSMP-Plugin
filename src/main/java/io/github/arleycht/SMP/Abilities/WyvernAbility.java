30adac877e0170aa60a7b9ac22c85e2cfcf82b18
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-03-22 12:58:25 -0400

    Add fireball to Wyvern ability

--- src/main/java/io/github/arleycht/SMP/Abilities/WyvernAbility.java
+++ src/main/java/io/github/arleycht/SMP/Abilities/WyvernAbility.java
@@ -1,24 +1,34 @@
 package io.github.arleycht.SMP.Abilities;
 
 import io.github.arleycht.SMP.Abilities.DeathMessage.DeathMessageManager;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import io.github.arleycht.SMP.util.Cooldown;
+import org.bukkit.*;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityToggleGlideEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.util.Vector;
 
 public class WyvernAbility extends Ability {
     public static final long TASK_INTERVAL_TICKS = 1L;
     public static final double RAIN_DAMAGE = 1.0;
     public static final long RAIN_DAMAGE_INTERVAL_TICKS = 40L;
+
+    public static final EntityDamageEvent.DamageCause[] DAMAGE_CAUSE_IMMUNITIES = {
+        EntityDamageEvent.DamageCause.FIRE,
+        EntityDamageEvent.DamageCause.FIRE_TICK,
+        EntityDamageEvent.DamageCause.LAVA
+    };
 
     public static final String[] DEATH_MESSAGES = {
             "{0} had their life extinguished by water",
@@ -26,11 +36,84 @@ public class WyvernAbility extends Ability {
             "{0} died as they begged the question"
     };
 
+    private Cooldown fireballCooldown = new Cooldown(15.0);
+
     private BukkitTask rainDamageTask = null;
 
     @Override
     public void initialize() {
         DeathMessageManager.setDeathMessages(this, DEATH_MESSAGES);
+    }
+
+    @EventHandler
+    public void onEntityDamageEvent(EntityDamageEvent event) {
+        Entity entity = event.getEntity();
+
+        if (!isOwner(entity)) {
+            return;
+        }
+
+        for (EntityDamageEvent.DamageCause cause : DAMAGE_CAUSE_IMMUNITIES) {
+            if (event.getCause() == cause) {
+                event.setCancelled(true);
+
+                entity.setFireTicks(0);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteractEvent(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        if (!isOwner(player) || !player.isSneaking()) {
+            return;
+        }
+
+        if (event.getAction() != Action.LEFT_CLICK_AIR) {
+            return;
+        }
+
+        if (event.getHand() != EquipmentSlot.HAND) {
+            return;
+        }
+
+        ItemStack handItem = event.getItem();
+
+        if (handItem == null || handItem.getType() != Material.FIRE_CHARGE) {
+            return;
+        }
+
+        // Activate ability
+
+        if (fireballCooldown.isNotReady()) {
+            return;
+        }
+
+        fireballCooldown.reset();
+
+        if (player.getGameMode() != GameMode.CREATIVE) {
+            handItem.setAmount(handItem.getAmount() - 1);
+        }
+
+        // Play sound
+
+        World world = player.getWorld();
+
+        world.playSound(player.getLocation(), Sound.ENTITY_BLAZE_SHOOT, 1.0f, 1.0f);
+
+        // Create fireball
+
+        Location location = player.getEyeLocation();
+        Vector direction = location.getDirection();
+
+        Fireball fireball = world.spawn(location, Fireball.class);
+
+        fireball.setShooter(player);
+        fireball.setDirection(direction);
+
+        fireball.setIsIncendiary(true);
+        fireball.setYield(2.0f);
     }
 
     @EventHandler

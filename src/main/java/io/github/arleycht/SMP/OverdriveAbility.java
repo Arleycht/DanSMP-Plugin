126b89adb99de99768e496dac5f54ce0d2608b10
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-03-28 18:59:35 -0400

    Update OverdriveAbility.java

--- src/main/java/io/github/arleycht/SMP/OverdriveAbility.java
+++ src/main/java/io/github/arleycht/SMP/OverdriveAbility.java
@@ -3,7 +3,13 @@ package io.github.arleycht.SMP;
 import io.github.arleycht.SMP.Abilities.Ability;
 
 public class OverdriveAbility extends Ability {
+    // TODO: Speed, Damage, Haste, True Damage
 
+    /*
+        Haste I -> 1.0 / s
+        Haste II ->
+        Speed
+     */
 
     @Override
     public String getName() {

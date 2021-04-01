a3f6b2f63662f928d405e472a0c9e35cad9dd0e9
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-04-01 13:10:52 -0400

    Create LightningAbility.java

---
+++ src/main/java/io/github/arleycht/SMP/Abilities/LightningAbility.java
@@ -0,0 +1,13 @@
+package io.github.arleycht.SMP.Abilities;
+
+public class LightningAbility extends Ability {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}

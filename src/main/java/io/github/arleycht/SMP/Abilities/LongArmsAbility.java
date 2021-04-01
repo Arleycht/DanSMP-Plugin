f4b3d94d477a5e6ed66903313015c977e20d4afb
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-04-01 13:10:22 -0400

    Create Long Arms and Punch ability classes

---
+++ src/main/java/io/github/arleycht/SMP/Abilities/LongArmsAbility.java
@@ -0,0 +1,18 @@
+package io.github.arleycht.SMP.Abilities;
+
+public class LongArmsAbility extends Ability {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "Walking Fortress";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Increased base armor and knockback resistance at the cost of movement speed.";
+    }
+}
---
+++ src/main/java/io/github/arleycht/SMP/Abilities/PunchAbility.java
@@ -0,0 +1,14 @@
+package io.github.arleycht.SMP.Abilities;
+
+public class PunchAbility extends Ability {
+
+    @Override
+    public String getName() {
+        return "One Punch";
+    }
+
+    @Override
+    public String getDescription() {
+        return "You know the deal.";
+    }
+}

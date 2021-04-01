f8e1ae868b0c742b9b8431f4ea486d01486fe7d6
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-04-01 13:09:53 -0400

    Update OverdriveAbility.java

--- src/main/java/io/github/arleycht/SMP/Abilities/OverdriveAbility.java
+++ src/main/java/io/github/arleycht/SMP/Abilities/OverdriveAbility.java
@@ -1,5 +1,12 @@
 package io.github.arleycht.SMP.Abilities;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+
 public class OverdriveAbility extends Ability {
     // TODO: Speed, Damage, Haste, True Damage
 
@@ -8,6 +15,32 @@ public class OverdriveAbility extends Ability {
         Haste II ->
         Speed
      */
+
+    @EventHandler
+    public void onPlayerInteractEvent(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        if (!isOwner(player)) {
+            return;
+        }
+
+        EquipmentSlot hand = event.getHand();
+
+        if (hand == null) {
+            return;
+        }
+
+        ItemStack item = player.getInventory().getItem(hand);
+
+        switch (item.getType()) {
+            case MUSIC_DISC_CAT:
+                Bukkit.getLogger().info("Overdrive CAT");
+            case MUSIC_DISC_WAIT:
+                Bukkit.getLogger().info("Overdrive WAIT");
+            default:
+                break;
+        }
+    }
 
     @Override
     public String getName() {

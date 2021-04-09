f9e581a73fc043e94b9f0fcff22802aad29e79b2
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-04-09 02:28:51 -0400

    Create GhostAbility.java

---
+++ src/main/java/io/github/arleycht/SMP/Abilities/GhostAbility.java
@@ -0,0 +1,13 @@
+package io.github.arleycht.SMP.Abilities;
+
+public class GhostAbility extends Ability {
+    @Override
+    public String getName() {
+        return "Literally a Ghost";
+    }
+
+    @Override
+    public String getDescription() {
+        return "F";
+    }
+}

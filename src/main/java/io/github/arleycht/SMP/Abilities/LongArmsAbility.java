e4ee32285f68ab872ece2df74393fec415331cc6
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-04-09 04:19:02 -0400

    Add temporary Long Arms ability
    Closes #5

--- src/main/java/io/github/arleycht/SMP/Abilities/LongArmsAbility.java
+++ src/main/java/io/github/arleycht/SMP/Abilities/LongArmsAbility.java
@@ -1,9 +1,16 @@
 package io.github.arleycht.SMP.Abilities;
 
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+
 public class LongArmsAbility extends Ability {
+    public static final double ADD_SPEED = 0.33;
+
     @Override
     public void initialize() {
+        AttributeModifier.Operation mul = AttributeModifier.Operation.ADD_SCALAR;
 
+        addAttributeModifier(Attribute.GENERIC_MOVEMENT_SPEED, ADD_SPEED, mul);
     }
 
     @Override
@@ -13,6 +20,6 @@ public class LongArmsAbility extends Ability {
 
     @Override
     public String getDescription() {
-        return "Increased ";
+        return "Increased speed lol";
     }
 }

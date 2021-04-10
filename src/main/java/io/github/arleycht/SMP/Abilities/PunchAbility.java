de98b56cb4ac3006bbf1680f86b82f09dcf470f6
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-04-10 15:47:52 -0400

    Rebalance punch

--- src/main/java/io/github/arleycht/SMP/Abilities/PunchAbility.java
+++ src/main/java/io/github/arleycht/SMP/Abilities/PunchAbility.java
@@ -1,18 +1,21 @@
 package io.github.arleycht.SMP.Abilities;
 
-import io.github.arleycht.SMP.util.Cooldown;
 import io.github.arleycht.SMP.util.Util;
-import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.BoundingBox;
 
 public class PunchAbility extends Ability {
-    public static final long TASK_INTERVAL_TICKS = 10L * 20L;
-    private final Cooldown ABILITY_COOLDOWN = new Cooldown(60.0 * 60.0);
+    public static final long TASK_INTERVAL_TICKS = 20L;
+
+    private long previousTime = 0;
+    private boolean active = true;
 
     @Override
     public boolean isRunnable() {
@@ -26,12 +29,33 @@ public class PunchAbility extends Ability {
 
     @Override
     public void run() {
-        if (ABILITY_COOLDOWN.isReady()) {
-            Player player = owner.getPlayer();
+        Player player = owner.getPlayer();
 
-            if (player != null) {
-                Util.applyEffect(player, PotionEffectType.GLOWING, 15.0f, 0, false, false, false);
-            }
+        if (player == null) {
+            return;
+        }
+
+        World world = player.getWorld();
+
+        long modTime = Math.floorMod(world.getTime(), 24000);
+
+        // Detect when time has looped
+        if (modTime < previousTime) {
+            active = true;
+        }
+
+        previousTime = modTime;
+
+        if (active) {
+            BoundingBox box = player.getBoundingBox();
+
+            double x = box.getWidthX() * 0.5;
+            double y = box.getHeight() * 0.5;
+            double z = box.getWidthZ() * 0.5;
+
+            Particle.DustOptions data = new Particle.DustOptions(Color.YELLOW, 0.5f);
+
+            world.spawnParticle(Particle.REDSTONE, player.getLocation(), 25, x, y, z, data);
         }
     }
 
@@ -53,11 +77,9 @@ public class PunchAbility extends Ability {
             return;
         }
 
-        if (ABILITY_COOLDOWN.isNotReady()) {
+        if (!active) {
             return;
         }
-
-        ABILITY_COOLDOWN.reset();
 
         event.setCancelled(true);
 
@@ -65,7 +87,7 @@ public class PunchAbility extends Ability {
 
         Util.dealTrueDamage(victim, victim.getHealth(), attacker);
 
-        Bukkit.broadcastMessage("ONE PAAAAAWWWWWNNNNNCH!");
+        active = false;
     }
 
     @Override

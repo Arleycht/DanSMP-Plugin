d549f0fd9925871a047ac78fd403b69fda0aa31c
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-04-09 20:36:46 -0400

    Create SlimeAbility.java

---
+++ src/main/java/io/github/arleycht/SMP/Abilities/SlimeAbility.java
@@ -0,0 +1,71 @@
+package io.github.arleycht.SMP.Abilities;
+
+import io.github.arleycht.SMP.Abilities.DeathMessage.DeathMessageManager;
+import io.github.arleycht.SMP.util.Util;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitTask;
+
+public class SlimeAbility extends Ability {
+    public static final long TASK_INTERVAL_TICKS = 10L;
+    public static final double WATER_DAMAGE = 1.0;
+    public static final long WATER_DAMAGE_INTERVAL_TICKS = 10L;
+    public static final String[] DEATH_MESSAGES = {
+            "{0} became too slimy",
+            "{0} doesn't like water",
+            "{0} died as they begged the question",
+            "{0} blubbed their last blub",
+            "{0} became fish food"
+    };
+
+    private BukkitTask waterDamageTask = null;
+
+    @Override
+    public void initialize() {
+        DeathMessageManager.setDeathMessages(this, DEATH_MESSAGES);
+    }
+
+    @Override
+    public boolean isRunnable() {
+        return true;
+    }
+
+    @Override
+    public long getTaskIntervalTicks() {
+        return TASK_INTERVAL_TICKS;
+    }
+
+    @Override
+    public void run() {
+        Player player = owner.getPlayer();
+
+        if (player == null) {
+            return;
+        }
+
+        Util.applyEffect(player, PotionEffectType.JUMP, 10.0f, 1, false, false, false);
+
+        if ((!Util.isInRain(player) && !Util.isInWater(player)) || player.isDead()) {
+            Util.safeTaskCancel(waterDamageTask);
+        } else if (Util.safeTaskIsCancelled(waterDamageTask)) {
+            waterDamageTask = Bukkit.getScheduler().runTaskTimer(getPlugin(), () -> {
+                if (WATER_DAMAGE >= player.getHealth()) {
+                    DeathMessageManager.setNextDeathMessage(player.getUniqueId(), this);
+                }
+
+                Util.dealTrueDamage(player, WATER_DAMAGE);
+            }, 0L, WATER_DAMAGE_INTERVAL_TICKS);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "Slime";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Slime";
+    }
+}

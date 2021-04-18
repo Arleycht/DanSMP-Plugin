102eab16efb0ef45fa17ec38a8f777f362342c29
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-04-18 02:33:40 -0400

    Create HuntAbility.java

---
+++ src/main/java/io/github/arleycht/SMP/Abilities/HuntAbility.java
@@ -0,0 +1,13 @@
+package io.github.arleycht.SMP.Abilities;
+
+public class HuntAbility extends Ability {
+    @Override
+    public String getName() {
+        return "The Hunt";
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}

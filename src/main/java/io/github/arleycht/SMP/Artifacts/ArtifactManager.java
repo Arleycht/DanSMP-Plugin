7aad37ddf85b971be9613281b57738567136eb0f
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-04-21 07:46:59 -0400

    Update ArtifactManager.java

--- src/main/java/io/github/arleycht/SMP/Artifacts/ArtifactManager.java
+++ src/main/java/io/github/arleycht/SMP/Artifacts/ArtifactManager.java
@@ -1,4 +1,23 @@
 package io.github.arleycht.SMP.Artifacts;
 
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.HashMap;
+
 public class ArtifactManager {
+    public static final HashMap<String, Artifact> ARTIFACT_MAP = new HashMap<String, Artifact>() {
+    };
+
+    public static void initialize() {
+
+    }
+
+    public static void tagItem(ItemStack itemStack, Artifact artifact) {
+        //itemStack
+
+        ItemMeta meta = itemStack.getItemMeta();
+
+
+    }
 }
